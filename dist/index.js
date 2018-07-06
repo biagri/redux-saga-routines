c77cb6247b76e9852b7994920b0e7a375e5be3cd
Martin Allard <m.allard@biagri.com>
2018-07-06 16:05:38 +0200

    Add forgotten build

--- dist/index.js
+++ dist/index.js
@@ -35,6 +35,12 @@ Object.defineProperty(exports, "bindPromiseCreators", {
     return _bindPromiseCreators.default;
   }
 });
+Object.defineProperty(exports, "withTracking", {
+  enumerable: true,
+  get: function get() {
+    return _withTracking.default;
+  }
+});
 Object.defineProperty(exports, "ROUTINE_PROMISE_ACTION", {
   enumerable: true,
   get: function get() {
@@ -51,5 +57,7 @@ var _createRoutine = _interopRequireDefault(require("./createRoutine"));
 var _promisifyRoutine = _interopRequireDefault(require("./promisifyRoutine"));
 
 var _bindPromiseCreators = _interopRequireDefault(require("./bindPromiseCreators"));
+
+var _withTracking = _interopRequireDefault(require("./withTracking"));
 
 var _constants = require("./constants");
--- es/index.js
+++ es/index.js
@@ -3,5 +3,6 @@ import bindRoutineToReduxForm from './bindRoutineToReduxForm';
 import createRoutine from './createRoutine';
 import promisifyRoutine from './promisifyRoutine';
 import bindPromiseCreators from './bindPromiseCreators';
+import withTracking from './withTracking';
 import { ROUTINE_PROMISE_ACTION } from './constants';
-export { routinePromiseWatcherSaga, bindRoutineToReduxForm, createRoutine, promisifyRoutine, bindPromiseCreators, ROUTINE_PROMISE_ACTION };
+export { routinePromiseWatcherSaga, bindRoutineToReduxForm, createRoutine, promisifyRoutine, bindPromiseCreators, withTracking, ROUTINE_PROMISE_ACTION };
